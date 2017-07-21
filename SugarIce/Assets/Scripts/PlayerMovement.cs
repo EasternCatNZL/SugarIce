0caf102102cccc6d8e32f7e2284a5d0287b87990
Chimpster <chimpsterchip@gmail.com>
2017-07-21 15:14:13 +1200

    Did a bit of work on the movement script

--- SugarIce/Assets/Scripts/PlayerMovement.cs
+++ SugarIce/Assets/Scripts/PlayerMovement.cs
@@ -1,16 +1,59 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using XInputDotNetPure;
 
-public class PlayerMovement : MonoBehaviour {
+public class PlayerMovement : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public PlayerIndex PlayerID;
+
+    private Vector3 Direction;
+    private Vector3 PrevDirection;
+
+    private bool PlayerConnected = false;
+    private GamePadState state;
+    private GamePadState prevState;
+
+    // Use this for initialization
+    void Start()
+    {
+        Direction = new Vector3(0.0f, 0.0f, 0.0f);
+        state = GamePad.GetState(PlayerID);
+        print(state.IsConnected);
+        if (!state.IsConnected)
+        {
+            Debug.LogError("Player " + PlayerID.ToString() + " is not connected");
+        }
+        else
+        {
+            PlayerConnected = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (PlayerConnected)
+        {
+            state = GamePad.GetState(PlayerID);
+            prevState = state;
+
+            Direction.x += state.ThumbSticks.Left.X;
+            Direction.z += state.ThumbSticks.Left.Y;
+
+            Direction.Normalize();
+
+            Debug.DrawRay(this.transform.position, Direction * 5, Color.red);
+
+            if (Vector3.Dot(transform.right, Direction) < 0.0f)
+            {
+                transform.Rotate(0.0f, -Vector3.Angle(transform.forward, Direction), 0.0f);
+            }
+            if (Vector3.Dot(transform.right, Direction) > 0.0f)
+            {
+                transform.Rotate(0.0f, Vector3.Angle(transform.forward, Direction), 0.0f);
+            }
+        }
+    }
 }
