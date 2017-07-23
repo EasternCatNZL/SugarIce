53d504d0c14e5ea9fbd4c24de4914c936a74b31f
Chimpster <chimpsterchip@gmail.com>
2017-07-23 13:32:27 +1200

    Finished Movement Script

--- SugarIce/Assets/Scripts/Player/PlayerMovement.cs
+++ SugarIce/Assets/Scripts/Player/PlayerMovement.cs
@@ -3,14 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using XInputDotNetPure;
 
+[RequireComponent(typeof(Rigidbody))]
+
 public class PlayerMovement : MonoBehaviour
 {
+    [Header("Movement Settings")]
+    public float Speed = 5.0f;
+    [Range(0,1)]
+    public float TurnRate = 0.2f; //Must be set between 1 - 0
 
+    [Header("Controller Settings")]
     public PlayerIndex PlayerID;
+    [Range(0,1)]
+    public float Deadzone = 0.1f;
 
     private Vector3 Direction;
     private Vector3 PrevDirection;
 
+    private Rigidbody Rigid;
     private bool PlayerConnected = false;
     private GamePadState state;
     private GamePadState prevState;
@@ -19,16 +29,18 @@ public class PlayerMovement : MonoBehaviour
     void Start()
     {
         Direction = new Vector3(0.0f, 0.0f, 0.0f);
+        //Check a controller is hooked up
         state = GamePad.GetState(PlayerID);
-        print(state.IsConnected);
         if (!state.IsConnected)
         {
-            Debug.LogError("Player " + PlayerID.ToString() + " is not connected");
+            Debug.LogWarning("Player " + PlayerID.ToString() + " is not connected");
         }
         else
         {
             PlayerConnected = true;
         }
+
+        Rigid = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -39,6 +51,7 @@ public class PlayerMovement : MonoBehaviour
             state = GamePad.GetState(PlayerID);
             prevState = state;
 
+            //Accumlate Direction so the model doesn't snap back
             Direction.x += state.ThumbSticks.Left.X;
             Direction.z += state.ThumbSticks.Left.Y;
 
@@ -46,14 +59,22 @@ public class PlayerMovement : MonoBehaviour
 
             Debug.DrawRay(this.transform.position, Direction * 5, Color.red);
 
+            //Player Rotations
             if (Vector3.Dot(transform.right, Direction) < 0.0f)
             {
-                transform.Rotate(0.0f, -Vector3.Angle(transform.forward, Direction), 0.0f);
+                transform.Rotate(0.0f, -Vector3.Angle(transform.forward, Direction) * TurnRate, 0.0f);
             }
             if (Vector3.Dot(transform.right, Direction) > 0.0f)
             {
-                transform.Rotate(0.0f, Vector3.Angle(transform.forward, Direction), 0.0f);
+                transform.Rotate(0.0f, Vector3.Angle(transform.forward, Direction) * TurnRate, 0.0f);
             }
+
+            //Move the player forward
+            if(state.ThumbSticks.Left.X + state.ThumbSticks.Left.Y > Deadzone || state.ThumbSticks.Left.X + state.ThumbSticks.Left.Y < -Deadzone)
+            {
+                Rigid.MovePosition(transform.position + transform.forward * Speed * Time.deltaTime);
+            }
+
         }
     }
 }
