c46f14381005a03202f4525471845f6b8339cbb5
EasternCatNZL <brianpwwong@gmail.com>
2017-07-25 23:40:36 +1200

    Started on order, havent thought of a good way to do it

---
+++ SugarIce/Assets/Scripts/Gameplay/OrderBehaviour.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrderBehaviour : MonoBehaviour {
+
+    //mixture array
+    private ChemicalBehaviour[] mixture = new ChemicalBehaviour[0];
+
+    //bools determining what is avalible
+    bool burnerAvalible = false;
+    bool tubeAvalible = false;
+    bool blueAvalible = false;
+    bool orangeAvalible = false;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //call at start of level, what is avalible to be used in orders
+    private void FindAvalibleTools()
+    {
+        //call from a level manager that is set for each level?
+    }
+
+    private void FindAvalibleChemicals()
+    {
+        //call from a level manager that is set for each level?
+    }
+}
---
+++ SugarIce/Assets/Scripts/Items/Chemical/ChemicalBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChemicalBehaviour : MonoBehaviour {
+
+    public enum DrugType
+    {
+        Blue,
+        Orange
+    }
+
+    private DrugType drugType;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public DrugType GetDrugType()
+    {
+        return drugType;
+    }
+}
